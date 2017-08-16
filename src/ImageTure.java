131b9e3f18a20cae2b31b0ebb43933f8f7cccc64
YuRacle <2441784417@qq.com>
2017-08-16 13:08:02 +0800

    文件

---
+++ src/ImageTure.java
@@ -0,0 +1,47 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by YuRacle on 2017/7/27.
+ * 图片路径为工程根目录
+ * 图片彩色转灰度
+ */
+
+public class ImageTure {
+
+    public static void main(String[] args) {
+        new ImageTure().turnToHui("753588.jpg","hui.jpg");
+    }
+//  实现方法
+    public void turnToHui(String path, String outPath) {
+        File file = new File(path);
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(file);
+            int width = image.getWidth();
+            int height = image.getHeight();
+            for (int i=0; i<width; i++) {
+                for (int j = 0; j < height; j++) {
+                    int rgb = image.getRGB(i, j);
+                    int r = (rgb&0xff0000)>>16;
+                    rgb = (r<<16)+(r<<8)+r;
+                    image.setRGB(i, j, rgb);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File file2 = new File(outPath);
+        try {
+            ImageIO.write(image, "jpg", file2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
